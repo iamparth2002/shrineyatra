bf661de4327ed31aaeffd8599eca7354ec422cd0
iamparth2002 <neenaparth@gmail.com>
2024-10-29 04:06:06 +0530

    next/images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['images.pexels.com','localhost','shrineyatra-backend.vercel.app'],
+      domains: ['images.pexels.com','localhost','shrineyatra-backend.vercel.app',"shrineyatra-backend.onrender.com"],
     },
   };
   
